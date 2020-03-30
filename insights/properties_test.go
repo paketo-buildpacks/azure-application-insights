c77c2a1cf95d498c613ba57b29a166716d83480c
Ben Hale <bhale@pivotal.io>
2020-03-30 10:32:59 -0700

    Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- insights/properties_test.go
+++ insights/properties_test.go
@@ -47,6 +47,11 @@ func testProperties(t *testing.T, context spec.G, it spec.S) {
 		Expect(err).NotTo(HaveOccurred())
 	})
 
+	it.After(func() {
+		Expect(os.RemoveAll(ctx.Buildpack.Path)).To(Succeed())
+		Expect(os.RemoveAll(ctx.Layers.Path)).To(Succeed())
+	})
+
 	it("contributes Properties", func() {
 		Expect(os.MkdirAll(filepath.Join(ctx.Buildpack.Path, "bin"), 0755)).To(Succeed())
 		Expect(ioutil.WriteFile(filepath.Join(ctx.Buildpack.Path, "bin", "azure-application-insights-properties"), []byte{}, 0644)).
