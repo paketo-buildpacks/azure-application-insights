170e51c2c10d6a7d7f8faf1129188d9eaa1a965f
Ben Hale <bhale@vmware.com>
2020-06-08 14:08:55 -0700

    profile.d scripts prevent startup on failure
    Signed-off-by: Ben Hale <bhale@vmware.com>

--- insights/properties.sh
+++ insights/properties.sh
@@ -1,4 +1,4 @@
 printf "Configuring Azure Application Insights properties\n"
 
 # shellcheck disable=SC2046
-eval $(azure-application-insights-properties)
+eval $(azure-application-insights-properties) || exit $?
